fea42865eb4aacb4e286142da6940af5c35eda5a
nestkatt25-afk <nestkatt25@gmail.com>
2026-06-22 21:35:24 -0400

    Update index.js

--- index.js
+++ index.js
@@ -265,12 +265,12 @@ client.on('messageCreate', async message => {
             .setDescription(
                 'I\'m too tired to come up with smth rn.'
             )
-            .setImage('attachment://bunnie bannner.png')
+            .setImage('attachment://bunniebannner.png')
             .setColor(0x7f8cff);
 
         return message.reply({
             embeds: [embed],
-            files: ['./images/bunnie bannner.png']
+            files: ['./images/bunniebannner.png']
         });
     }
 
